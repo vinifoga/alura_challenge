c3e755e0f51827ccef0720d8d89d04afc3c71023
Vinicius Fernandes Fogaça <viniciusfernandes38@gmail.com>
2022-10-09 13:22:40 -0300

    working with rows and column

--- our_first_project/lib/main.dart
+++ our_first_project/lib/main.dart
@@ -15,23 +15,65 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       home: Container(
-          color: Colors.white,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Container(
-                color: Colors.red,
-                width: 150,
-                height: 150,
-              ),
-              Container(
-                color: Colors.blue,
-                width: 75,
-                height: 75,
-              ),
-            ],
-          )),
+        color: Colors.white,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Stack(
+              alignment: AlignmentDirectional.center,
+              children: [
+                Container(
+                  color: Colors.red,
+                  width: 100,
+                  height: 100,
+                ),
+                Container(
+                  color: Colors.blue,
+                  width: 50,
+                  height: 50,
+                ),
+              ],
+            ),
+            Stack(
+              alignment: AlignmentDirectional.center,
+              children: [
+                Container(
+                  color: Colors.blue,
+                  width: 100,
+                  height: 100,
+                ),
+                Container(
+                  color: Colors.red,
+                  width: 50,
+                  height: 50,
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Container(
+                  color: Colors.cyan,
+                  width: 50,
+                  height: 50,
+                ),
+                Container(
+                  color: Colors.pinkAccent,
+                  width: 50,
+                  height: 50,
+                ),
+                Container(
+                  color: Colors.purple,
+                  width: 50,
+                  height: 50,
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
     );
   }
 }
