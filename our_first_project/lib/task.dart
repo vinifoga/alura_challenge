4db57b173211fce49d18689a78f64f9ff70fa32b
Vinicius Fernandes Fogaça <viniciusfernandes38@gmail.com>
2022-10-15 21:00:51 -0300

    stateful widget and setstate

--- our_first_project/lib/task.dart
+++ our_first_project/lib/task.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
 
-class Task extends StatelessWidget {
+class Task extends StatefulWidget {
   final String name;
   const Task(this.name, {Key? key}) : super(key: key);
 
+  @override
+  State<Task> createState() => _TaskState();
+}
+
+class _TaskState extends State<Task> {
+  int nivel = 0;
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -14,29 +20,39 @@ class Task extends StatelessWidget {
             color: Colors.blue,
             height: 140,
           ),
-          Container(
-            color: Colors.white,
-            height: 100,
-            child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Container(
-                    color: Colors.black26,
-                    width: 72,
-                    height: 100,
-                  ),
-                  SizedBox(
-                    width: 200,
-                    child: Text(
-                      name,
-                      style: const TextStyle(
-                          fontSize: 22, overflow: TextOverflow.ellipsis),
-                    ),
-                  ),
-                  ElevatedButton(
-                      onPressed: () {},
-                      child: const Icon(Icons.arrow_drop_down))
-                ]),
+          Column(
+            children: [
+              Container(
+                color: Colors.white,
+                height: 100,
+                child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Container(
+                        color: Colors.black26,
+                        width: 72,
+                        height: 100,
+                      ),
+                      SizedBox(
+                        width: 200,
+                        child: Text(
+                          widget.name,
+                          style: const TextStyle(
+                              fontSize: 22, overflow: TextOverflow.ellipsis),
+                        ),
+                      ),
+                      ElevatedButton(
+                          onPressed: () {
+                            setState(() {
+                              nivel++;
+                            });
+                          },
+                          child: const Icon(Icons.arrow_drop_down))
+                    ]),
+              ),
+              Text('Nivel : $nivel',
+                  style: const TextStyle(color: Colors.white))
+            ],
           ),
         ],
       ),
