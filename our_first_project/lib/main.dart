e66d0532f18a74349ea1d19a77a8315432e99d9b
Vinicius Fernandes Fogaça <viniciusfernandes38@gmail.com>
2022-10-16 20:05:22 -0300

    working with widget Image

--- our_first_project/lib/main.dart
+++ our_first_project/lib/main.dart
@@ -16,28 +16,30 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          leading: Container(),
+          title: const Text('Tasks'),
         ),
-        home: Scaffold(
-            appBar: AppBar(
-              leading: Container(),
-              title: const Text('Tasks'),
-            ),
-            body: ListView(
-              children: const [
-                Task('Aprendendo Flutter'),
-                Task('Aprendendo Flutter'),
-                Task('Aprendendo Flutter'),
-                Task('Aprendendo Flutter'),
-                Task('Aprendendo Flutter'),
-                Task('Aprendendo Flutter'),
-                Task('Aprendendo Flutter'),
-                Task('Aprendendo Flutter'),
-                Task('Aprendendo Flutter'),
-                Task('Aprendendo Flutter'),
-              ],
-            )));
+        body: ListView(
+          children: [
+            const Task('Aprender Flutter',
+                'https://pbs.twimg.com/media/Eu7m692XIAEvxxP?format=png&name=large'),
+            Task('Andar de Bike',
+                'https://tswbike.com/wp-content/uploads/2020/09/108034687_626160478000800_2490880540739582681_n-e1600200953343.jpg'),
+            Task('Meditar',
+                'https://manhattanmentalhealthcounseling.com/wp-content/uploads/2019/06/Top-5-Scientific-Findings-on-MeditationMindfulness-881x710.jpeg'),
+            Task('Ler',
+                'https://thebogotapost.com/wp-content/uploads/2017/06/636052464065850579-137719760_flyer-image-1.jpg'),
+            Task('Jogar',
+                'https://i.ibb.co/tB29PZB/kako-epifania-2022-2-c-pia.jpg'),
+          ],
+        ),
+      ),
+    );
   }
 }
