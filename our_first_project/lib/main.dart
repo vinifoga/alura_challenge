10b29e51f07ebedf8c00c2ee208bdf88f7be2a8c
Vinicius Fernandes Fogaça <viniciusfernandes38@gmail.com>
2022-10-17 20:44:22 -0300

    working with animatedOpacity

--- our_first_project/lib/main.dart
+++ our_first_project/lib/main.dart
@@ -21,13 +21,20 @@ class _MyAppState extends State<MyApp> {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: WidgetStrikesBackChallenge(),
+      home: MainScaffold(),
     );
   }
 }
 
-class MainScaffold extends StatelessWidget {
-  const MainScaffold({Key? key}) : super(key: key);
+class MainScaffold extends StatefulWidget {
+  MainScaffold({Key? key}) : super(key: key);
+
+  @override
+  State<MainScaffold> createState() => _MainScaffoldState();
+}
+
+class _MainScaffoldState extends State<MainScaffold> {
+  bool taskOpacity = true;
 
   @override
   Widget build(BuildContext context) {
@@ -36,28 +43,40 @@ class MainScaffold extends StatelessWidget {
         leading: Container(),
         title: const Text('Tasks'),
       ),
-      body: ListView(
-        // ignore: prefer_const_literals_to_create_immutables
-        children: [
-          const Task(
-              'Aprender Flutter',
-              'https://pbs.twimg.com/media/Eu7m692XIAEvxxP?format=png&name=large',
-              3),
-          const Task(
-              'Andar de Bike',
-              'https://tswbike.com/wp-content/uploads/2020/09/108034687_626160478000800_2490880540739582681_n-e1600200953343.jpg',
-              2),
-          const Task(
-              'Meditar',
-              'https://manhattanmentalhealthcounseling.com/wp-content/uploads/2019/06/Top-5-Scientific-Findings-on-MeditationMindfulness-881x710.jpeg',
-              5),
-          const Task(
-              'Ler',
-              'https://thebogotapost.com/wp-content/uploads/2017/06/636052464065850579-137719760_flyer-image-1.jpg',
-              4),
-          const Task('Jogar',
-              'https://i.ibb.co/tB29PZB/kako-epifania-2022-2-c-pia.jpg', 1),
-        ],
+      body: AnimatedOpacity(
+        opacity: taskOpacity ? 1 : 0,
+        duration: Duration(milliseconds: 800),
+        child: ListView(
+          // ignore: prefer_const_literals_to_create_immutables
+          children: [
+            const Task(
+                'Aprender Flutter',
+                'https://pbs.twimg.com/media/Eu7m692XIAEvxxP?format=png&name=large',
+                3),
+            const Task(
+                'Andar de Bike',
+                'https://tswbike.com/wp-content/uploads/2020/09/108034687_626160478000800_2490880540739582681_n-e1600200953343.jpg',
+                2),
+            const Task(
+                'Meditar',
+                'https://manhattanmentalhealthcounseling.com/wp-content/uploads/2019/06/Top-5-Scientific-Findings-on-MeditationMindfulness-881x710.jpeg',
+                5),
+            const Task(
+                'Ler',
+                'https://thebogotapost.com/wp-content/uploads/2017/06/636052464065850579-137719760_flyer-image-1.jpg',
+                4),
+            const Task('Jogar',
+                'https://i.ibb.co/tB29PZB/kako-epifania-2022-2-c-pia.jpg', 1),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            taskOpacity = !taskOpacity;
+          });
+        },
+        child: const Icon(Icons.remove_red_eye),
       ),
     );
   }
